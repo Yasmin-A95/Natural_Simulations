cef02efd2eb8351e70fb1808c34d47ec9aaf0182
Yasmin-A95 <107013100+Yasmin-A95@users.noreply.github.com>
2022-10-23 22:21:58 +1100

    made canvas viewport sized, added basic functionality for walker drawing

--- scripts.js
+++ scripts.js
@@ -1,37 +1,49 @@
-const Walker = function () {
-    this.x = width / 2;
-    this.y = width / 2;
-};
-
-Walker.prototype.display = function () {
-    stroke(0, 0, 0);
-    point(this.x, this.y);
-};
-
-Walker.prototype.walk = function () {
-    let choice = Math.floor(Math.random() * 5);
-    if (choice === 0) {
-        this.x++;
-    } else if (choice === 1) {
-        this.x--;
-    } else if (choice === 2) {
-        this.y++;
-    } else {
-        this.y--;
-    }
-};
-
-const w = new Walker();
-
-draw = function () {
-    w.walk();
-    w.display();
-};
-
-canvas = document.getElementById('canvas');
-ctx = canvas.getContext('2d')
 
 document.addEventListener("DOMContentLoaded", function () {
     console.log("dom content loaded")
+    canvas = document.getElementById('canvas');
+    ctx = canvas.getContext('2d')
+    const width = canvas.width;
+    const height = canvas.height;
+
+    const Walker = function () {
+        this.x = width / 2;
+        this.y = height / 2;
+    };
+
+    Walker.prototype.display = function () {
+        ctx.beginPath();
+        console.log(this.x, this.y)
+        ctx.moveTo(this.x, this.y)
+        ctx.lineTo(this.x + 5, this.y + 5)
+        ctx.stroke();
+        console.log(this.x++, this.y++, " this plus plus")
+        //setInterval(w.display, 3000)
+    };
+
+    Walker.prototype.walk = function () {
+        let choice = Math.floor(Math.random() * 5);
+        if (choice === 0) {
+            this.x++;
+        } else if (choice === 1) {
+            this.x--;
+        } else if (choice === 2) {
+            this.y++;
+        } else {
+            this.y--;
+        }
+        console.log(choice)
+    };
+
+    const w = new Walker();
+
+    draw = function () {
+        w.walk();
+        w.display();
+    };
+    draw()
 })
+
+
+
 
